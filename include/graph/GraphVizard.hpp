a2dda13c94c7b55088cd06b8878c0d0bb2806e57
Melnikov Denis <melnikov.denis.aleksandrovich@gmail.com>
2026-02-27 22:32:58 +0300

    [Tests]: Add tests for 7 instructions
    Add tests for building graphs
    of single nodes with only one instructions.
    Check the number of input/output paraneters
    and instruction name.
    
    [CI]: Add footjob and running tests
    
    Add new job - footjob
    Also add running tests

--- include/graph/GraphVizard.hpp
+++ include/graph/GraphVizard.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <string>
+
 #include "graph/Graph.hpp"
 
 namespace GraphVizardry {
+
+static const std::string DOT_LOG_FILE_NAME = "log/dot.dot";
+
 void dumpGraphViz(const GiantGraph::GiantGraph &GiantGraph);
+
 } // namespace GraphVizardry
